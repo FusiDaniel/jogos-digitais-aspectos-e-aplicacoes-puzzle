fb523ce6f645a4849a75d0f02c4d4c3c1d340098
FusiDaniel <fpires.daniel@gmail.com>
2023-10-08 19:02:12 -0300

    part 2 without testing

---
+++ Assets/Scripts/chamaAbertura.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class chamaAbertura : MonoBehaviour
+{
+    public void AbrirGame() {
+        SceneManager.LoadScene("Abertura");
+    }
+}
