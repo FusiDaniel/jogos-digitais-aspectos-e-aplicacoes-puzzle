f187fa74dd615a676aa1e98e11d09ff3aa04c309
FusiDaniel <fpires.daniel@gmail.com>
2023-10-08 18:37:50 -0300

    part 2 step 52

---
+++ Assets/Scripts/scrollTexto.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollTexto : MonoBehaviour
+{
+    public float scrollSpeed = 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;;
+        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+        pos += localVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
